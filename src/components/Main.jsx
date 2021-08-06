ff0c1957c9cd0446fccf469b62d3aa06c938402b
disaisoft <disaisoft@gmail.com>
2021-08-06 13:47:48 -0500

    actualización de numeros de telefonos

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -20,7 +20,7 @@ const Main = () => {
                                 <p className="d-none d-md-block">En caso de urgencia que implique prótesis: contamos con laboratorio propio
                                 Para procedimientos y toma de Radiografías en un sólo lugar.
                                 </p>
-                                <a href="tel:+573015343777" className="btn btn-outline-light">Llamar - Opción 1 - 3015343777</a><br /><br />
+                                <a href="tel:+573002161403" className="btn btn-outline-light">Llamar - Opción 1 - 3002161403</a><br /><br />
                                 <a href="tel:+573002161403" className="btn btn-outline-light">Llamar - Opción 2 - 3002161403</a>
                             </div>
                         </div>
